36421c7cbe6be8f4ed53f125c7733648082e10be
Mitchell Simoens <mitchellsimoens@gmail.com>
2017-04-11 19:29:44 -0400

    Get packages to load via Router

--- app/App/app/view/main/MainController.js
+++ app/App/app/view/main/MainController.js
@@ -8,8 +8,15 @@ Ext.define('App.view.main.MainController', {
     extend: 'Ext.app.ViewController',
 
     requires: ['Ext.Package'],
-    
+
     alias: 'controller.main',
+
+    routes: {
+        'type/:type': {
+            before: 'loadPackage',
+            action: 'showView'
+        }
+    },
 
     onItemSelected: function (sender, record) {
         Ext.Msg.confirm('Confirm', 'Are you sure?', 'onConfirm', this);
@@ -21,19 +28,44 @@ Ext.define('App.view.main.MainController', {
         }
     },
 
-    gotoPage: function(type, tabContainer) {
-        if (type != 'Home') {
-            if (!Ext.Package.isLoaded(type)) {
-                Ext.Package.load(type).then(function(){
-                    var clsName = 'App.view.main.' + type + "List";
-                    tabContainer.add(Ext.create(clsName, {}));
+    loadPackage: function (type, action) {
+        if (type === 'Home' || Ext.Package.isLoaded(type)) {
+            action.resume();
+        } else {
+            var tabpanel = this.getView();
+
+            tabpanel.setMasked({
+                message: 'Loading Package...'
+            });
+
+            Ext.Package.load(type).then(function () {
+                tabpanel.setMasked(null);
+
+                action.resume();
+            });
+        }
+    },
+
+    showView: function (type) {
+        var tabpanel = this.getView(),
+            tab = tabpanel.child('[title=' + type + ']');
+
+        tabpanel.setActiveItem(tab);
+
+        if (type !== 'Home') {
+            if (!tab.hasItem) {
+                tab.hasItem = true;
+
+                tab.add({
+                    xclass: 'App.view.main.' + type + 'List'
                 });
             }
         }
     },
-    
-    onItemActivate: function(sender, value, oldValue, eOpts) {
-        var pkgName = value.title;
-        this.gotoPage(pkgName, value);
+
+    onItemActivate: function(tabpanel, tab) {
+        var pkgName = tab.title;
+
+        this.redirectTo('type/' + pkgName);
     }
 });
